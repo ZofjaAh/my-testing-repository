10e5b9d326dd1de009f8eb1b7ed5b2b3b33e9c68
ZozoLulu <Zozolulu1894@gmail.com>
2023-12-29 21:10:30 +0100

    Person class modification 1

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -1,14 +1,22 @@
 package pl.zajavka.case2;
-
 import java.math.BigDecimal;
-
-record Person(String name, BigDecimal salary) {
-
+record Person(String name, Long age, BigDecimal salary) {
     public BigDecimal howMuchDoYouEarn() {
         return salary;
     }
 
     public Boolean doYouLikePizza() {
+        return true;
+    }
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
+    }
+
+    public Boolean doYouHaveABike() {
         return true;
     }
 }
